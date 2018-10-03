08e5fd83dff570a11a7292dc90c2266a579c35a7
Bishal Gautam <bishalkrgautam@gmail.com>
2018-10-03 17:22:50 -0500

    filling select in html from property binding and *ngFor

--- src/app/employees/create-employees.component.ts
+++ src/app/employees/create-employees.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Department } from '../models/department.model'
 
 @Component({
   selector: 'app-create-employees',
@@ -8,15 +9,26 @@ import { NgForm } from '@angular/forms';
 })
 export class CreateEmployeesComponent implements OnInit {
 
-  department='2';//select option 2 by default from select list
-  gender='male'; // this line checks the gender radio button to male by default
-  isActive= false; // to check the checkbox by default
-  constructor() { }
+  // department='2';//select option 2 by default from select list
+  // gender='male'; // this line checks the gender radio button to male by default
+  // isActive= false; // to check the checkbox by default
+  // constructor() { }
+
+
+  //define departments properties to dynamically populate select in .html
+  departments: Department[] = [
+    { id: 1, name: 'IT' },
+    { id: 2, name: 'HR' },
+    { id: 3, name: 'Help Desk' },
+    { id: 4, name: 'Payroll' },
+    {id:5, name:'Admin'}
+  ];
+
 
   ngOnInit() {
   }
 
-  saveEmployee(empForm:NgForm){
+  saveEmployee(empForm: NgForm) {
     console.log(empForm);
   }
 }
