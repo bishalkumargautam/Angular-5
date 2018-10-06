eec0e37b57317a291fae0e1e15ab758babb530c3
Bishal Gautam <bishalkrgautam@gmail.com>
2018-10-05 19:46:31 -0500

    Detecting and reacting to Input property changes using Property Setter

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,5 +1,5 @@
 import { Employee } from './../models/employee.model';
-import { Component, OnInit, Input, OnChanges,  SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-display-employee',
@@ -8,20 +8,34 @@ import { Component, OnInit, Input, OnChanges,  SimpleChanges } from '@angular/co
 })
 export class DisplayEmployeeComponent implements OnInit {
 
-  @Input() employee:Employee;
+  //change detection using setter and getter method
+  private _employee: Employee;
+  @Input()
+  set employee(value: Employee) {
+    console.log('Previous : ' + (this._employee ? this._employee.name : 'Null'));
+    this._employee = value;
+    console.log('Current : ' + this._employee.name);
+    
+
+  }
+  get employee(): Employee {
+    return this._employee;
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
-  
- // This life cycle hook receives SimpleChanges as an Input parameter
-// We can use it to retrieve previous and current values as shown below
-ngOnChanges(changes: SimpleChanges) {
-  const previousEmployee = <Employee>changes.employee.previousValue;
-  const currentEmployee = <Employee>changes.employee.currentValue;
 
-  console.log('Previous : ' + (previousEmployee ? previousEmployee.name : 'NULL') );
-  console.log('Current : ' + currentEmployee.name);
-}
+
+
+  // // This life cycle hook receives SimpleChanges as an Input parameter
+  // // We can use it to retrieve previous and current values as shown below
+  // ngOnChanges(changes: SimpleChanges) {
+  //   const previousEmployee = <Employee>changes.employee.previousValue;
+  //   const currentEmployee = <Employee>changes.employee.currentValue;
+
+  //   console.log('Previous : ' + (previousEmployee ? previousEmployee.name : 'NULL'));
+  //   console.log('Current : ' + currentEmployee.name);
+  // }
 }
